0749858410017819338545aa8fba7e0ba0f09ecb
Tomas Okal <tomas.okal@gmail.com>
2019-04-07 16:13:18 -0500

    Scratch function.

--- R/scratch.R
+++ R/scratch.R
@@ -15,10 +15,55 @@ full_data[, 2:ncol(full_data)][full_data[, 2:ncol(full_data)] == 77] <- NA
 full_data[, 2:ncol(full_data)][full_data[, 2:ncol(full_data)] == 98] <- NA
 full_data[, 2:ncol(full_data)][full_data[, 2:ncol(full_data)] == 99] <- NA
 
+#export
+export(full_data, "full_data.rds")
+export(full_data, "full_data.dta")
+
 #survey setup
 survey_data <- full_data %>%
+  mutate(q41_binary = ifelse(q41 > 2,
+                             1,
+                             0)) %>%
   as_survey_design(weights = finalwt, id = su_id)
 
-m <- svyolr(factor(q41) ~ politics + factor(raceth), survey_data)
+#test
+predictorlist<- list("x1","x2")
+for (i in predictorlist){
+  model <- lm(paste("y ~", i[[1]]), data=df)
+  print(summary(model))
+}
+
+covariates <- list("census_region", "gender", "q42")
+for (i in covariates) {
+  olr_models <- svyolr(paste("factor(q41) ~ factor(politics) + urban + agegrp + education + hhincome +", i[[1]]), survey_data)
+  print(summary(olr_models))
+}
+
+
+#Ordinal Logit
+m <- svyolr(factor(q41) ~ factor(politics) + urban + agegrp + education + hhincome + gender , survey_data)
 tidym <- tidy(m)
 tidym$p <- round((pnorm(abs(tidym$statistic), lower.tail = FALSE) * 2), digits = 5)
+
+tidym <- tidym %>%
+  mutate(mvr = ifelse(p > 0.05,
+                      "",
+                      ifelse(estimate > 0,
+                             "+",
+                             "-")
+                      )
+         ) %>%
+  filter(coefficient_type == "coefficient")
+
+#Logit
+survey_data$variables$q41_binary
+glm <- svyglm(q41_binary ~ urban + agegrp + education + hhincome + gender, family = binomial, survey_data)
+tidyglm <- tidy(glm) %>%
+  mutate(mvr = ifelse(p.value > 0.05,
+                      "",
+                      ifelse(estimate > 0,
+                             "+",
+                             "-")
+                      )
+  )
+
