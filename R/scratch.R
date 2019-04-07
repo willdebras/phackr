c940ed3ead45ff2293313a7b31983dc1fedfc684
Tomas Okal <tomas.okal@gmail.com>
2019-04-07 18:14:08 -0500

    Working loop to create list of list of tidy survey ologit results.

--- R/scratch.R
+++ R/scratch.R
@@ -1,4 +1,4 @@
-#packages
+#Necessary packages
 library(tidyverse)
 library(survey)
 library(srvyr)
@@ -6,37 +6,49 @@ library(rio)
 library(mitools)
 library(broom)
 
+#Stop the war crime of scientific notation
 options(scipen=10)
 
-#import
+#Import dataset
 full_data <- import("T:\\saved_downloads\\apnorc_thanksgiving_nov2017.dta")
 full_data[, 2:ncol(full_data)][full_data[, 2:ncol(full_data)] == -99] <- NA
 full_data[, 2:ncol(full_data)][full_data[, 2:ncol(full_data)] == 77] <- NA
 full_data[, 2:ncol(full_data)][full_data[, 2:ncol(full_data)] == 98] <- NA
 full_data[, 2:ncol(full_data)][full_data[, 2:ncol(full_data)] == 99] <- NA
 
-#export
-export(full_data, "full_data.rds")
-export(full_data, "full_data.dta")
-
-#survey setup
+#Setup the survey object
 survey_data <- full_data %>%
   mutate(q41_binary = ifelse(q41 > 2,
                              1,
                              0)) %>%
   as_survey_design(weights = finalwt, id = su_id)
 
-#test2
-covariates <- list("census_region", "gender", "q42")
-results <- list()
+#Create list of list of regression outputs
+demographics <- "~ factor(politics) + urban + agegrp + education + hhincome +"
+dv <- list("factor(q41)", "factor(q42)")
+covariates <- list("census_region", "gender")
+dv_models <- list()
+covariates_models <- list()
+
 for (i in covariates) {
-  olr_models <- svyolr(paste("factor(q41) ~ factor(politics) + urban + agegrp + education + hhincome +", i[[1]]), survey_data)
-  results[[i]] <- tidy(olr_models[[i]])
+
+  for (j in dv) {
+
+    dv_models[[j]] <- tidy(svyolr(paste(j[[1]], demographics, i[[1]]), survey_data))
+
+  }
+
+ covariates_models[[i]] <- dv_models
+
 }
 
+##########################
+#Working Ologit and Logit#
+##########################
 
 #Ordinal Logit
-m <- svyolr(factor(q41) ~ factor(politics) + urban + agegrp + education + hhincome + gender , survey_data)
+m <- svyolr(factor(q42) ~ factor(politics) + urban + agegrp + education + hhincome + gender, survey_data)
+tidy(m)
 tidym <- tidy(m)
 tidym$p <- round((pnorm(abs(tidym$statistic), lower.tail = FALSE) * 2), digits = 5)
 
@@ -51,8 +63,8 @@ tidym <- tidym %>%
   filter(coefficient_type == "coefficient")
 
 #Logit
-survey_data$variables$q41_binary
 glm <- svyglm(q41_binary ~ urban + agegrp + education + hhincome + gender, family = binomial, survey_data)
+
 tidyglm <- tidy(glm) %>%
   mutate(mvr = ifelse(p.value > 0.05,
                       "",
