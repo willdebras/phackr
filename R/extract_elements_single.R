15009fab44bae96e3fd8f6a64dbae37a2c57d963
Will Bonnell <wtb3na@virginia.edu>
2019-04-11 10:02:23 -0500

    adding extract elements to helper function

---
+++ R/extract_elements_single.R
@@ -0,0 +1,26 @@
+#' extract_elements
+#'
+#' This is a backend helper function to interact with the phackr function.
+#'
+#' @param x
+#'
+#' @return a single column tibble indicating significance and directionality of an ordinal logit regression output
+#' @export
+#' @import dplyr
+#' @importFrom pluck purrr
+#'
+extract_elements_single <- function(x) {
+
+  data.frame(pluck(dv_models, x)) %>%
+    filter(coefficient_type == "coefficient") %>%
+    mutate(p = round((pnorm(abs(statistic), lower.tail = FALSE) * 2), digits = 5),
+           mvr = ifelse(p > 0.05,
+                        "",
+                        ifelse(estimate > 0,
+                               "+",
+                               "-")
+           )
+    ) %>%
+    select(mvr)
+
+}
