6c16e97975ebfdc24b03c491d766e8d0f56a1578
Tomas Okal <tomas.okal@gmail.com>
2019-04-11 19:22:03 -0500

    Added logistic regression option. Tentative functionality.

---
+++ R/extract_elements_ologit_single.R
@@ -0,0 +1,27 @@
+#' extract_elements
+#'
+#' This is a backend helper function to interact with the phackr function.
+#'
+#' @param x
+#'
+#' @return a single column tibble indicating significance and directionality of an ordinal logit regression output
+#' @export
+#' @import dplyr
+#' @importFrom purrr pluck
+#' @import magrittr
+#'
+extract_elements_single <- function(dv_models, x) {
+
+  data.frame(pluck(dv_models, x)) %>%
+    filter(coefficient_type == "coefficient") %>%
+    mutate(p = round((pnorm(abs(statistic), lower.tail = FALSE) * 2), digits = 5),
+           mvr = ifelse(p > 0.05,
+                        "",
+                        ifelse(estimate > 0,
+                               "+",
+                               "-")
+           )
+    ) %>%
+    select(mvr)
+
+}
--- R/phackr.R
+++ R/phackr.R
@@ -5,10 +5,11 @@
 #' @param data A survey data object. This can be created with the phackr_setup function
 #' @param dvs A vector of dependent variables to run the same model across.
 #' @param demos A vector of demographic variables and covariates to include in the ologit model.
+#' @param logit A parameter to run logistic regressions.
 #'
 #' @return Returns a tibble that can quickly be written to an excel sheet
 #' @export
-#' @importFrom survey svyolr
+#' @importFrom survey svyolr svyglm
 #' @importFrom broom tidy
 #' @import tibble
 #' @import magrittr
@@ -16,23 +17,45 @@
 #' @examples
 #' sheet1 <- phackr(data = survey_data, dvs = c("q2", "q3", "q4"), demos = c("race", "partyid", "gender"))
 #'
-phackr <- function(data, dvs, demos) {
+phackr <- function(data, dvs, demos, logit = FALSE) {
 
   ###create empty lists to fill
   dv_models <- list()
 
   dv <- as.list(dvs)
 
-  for (i in dv) {
+  if (logit) {
 
-    dv_models[[i]] <- tidy(svyolr(paste(i[[1]], paste("~ ", paste(demos, collapse = " + "))), data))
+    for (i in dv) {
+
+      dv_models[[i]] <- tidy(svyglm(paste(i[[1]], paste("~ ", paste(demos, collapse = " + "))), family = quasibinomial, data))
+
+    }
+
+    sheet <- tibble(rows = demos)
+
+    for (i in 1:length(dv_models)) {
+
+      sheet[(i+1)] <- add_column(extract_elements_logit_single(dv_models, i))
+
+    }
 
   }
-  sheet <- tibble(rows = demos)
 
-  for (i in 1:length(dv_models)) {
+  else {
 
-    sheet[(i+1)] <- add_column(extract_elements_single(dv_models, i))
+    for (i in dv) {
+
+      dv_models[[i]] <- tidy(svyolr(paste(i[[1]], paste("~ ", paste(demos, collapse = " + "))), data))
+
+    }
+    sheet <- tibble(rows = demos)
+
+    for (i in 1:length(dv_models)) {
+
+      sheet[(i+1)] <- add_column(extract_elements_ologit_single(dv_models, i))
+
+    }
 
   }
 
@@ -40,7 +63,6 @@ phackr <- function(data, dvs, demos) {
     remove_rownames %>%
     column_to_rownames(var="rows") %>%
     `colnames<-`(unlist(dv, use.names = FALSE))
-
 
 }
 
